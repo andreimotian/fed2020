4b6d8814e25bc99b7fbca2ce6b9e41366c98b9df
andreimotian <31732261+andreimotian@users.noreply.github.com>
2020-09-23 23:56:08 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,14 @@
+//jslint browser:true, devel:true, eqeq:true, plusplus:true, sloppy:true, vars:true, white:true//
+
 // JavaScript Document
 
-var navigatie   = document.querySelector('header > nav')
-var menuKnop    = document.querySelector('header > button');
+var navigatie = document.querySelector('header > nav');
+var menuKnop = document.querySelector('header > button');
 
-var open = function () {
-  navigatie.classList.toggle('open')
+var open = function() {
+    navigatie.classList.toggle('open');
+    menuKnop.classList.toggle('animated');
+
 }
 
 menuKnop.addEventListener('click', open);
